c36eb98e6a20f58d7ca7be82c0b64edacf66c37b
Shuvo <iosguybd@gmail.com>
2018-11-04 14:47:08 +0600

    Initial Commit

---
+++ MyQuotes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyQuotes
+//
+//  Created by Md Zahidul Islam Mazumder on 4/11/18.
+//  Copyright © 2018 Md Zahidul islam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
